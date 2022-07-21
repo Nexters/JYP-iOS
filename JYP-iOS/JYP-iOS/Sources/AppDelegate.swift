b4b15e412c52c68ed15d46d5560f683826473ade
송영모 <mooyoung2309@gmail.com>
2022-07-22 04:18:22 +0900

    docs: 주석 정리

--- JYP-iOS/JYP-iOS/Sources/AppDelegate.swift
+++ JYP-iOS/JYP-iOS/Sources/AppDelegate.swift
@@ -14,16 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        print("1")
-        // Override point for customization after application launch.
-//        window = UIWindow(frame: UIScreen.main.bounds)
-        // FIXME: 카카오 SDK 설정
-//        KakaoSDK.initSDK(appKey: "여기다가 앱키!!")
-//        let mainViewController = TabBarViewController()
-//
-//        window?.rootViewController = mainViewController
-//        window?.makeKeyAndVisible()
-//
+        KakaoSDK.initSDK(appKey: "여기다가 앱키!!")
         return true
     }
     
