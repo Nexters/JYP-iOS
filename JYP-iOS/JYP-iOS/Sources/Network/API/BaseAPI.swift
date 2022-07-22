89a706b3af23549d2dc93a7c5cd2d7122667f8f3
inae <inae.lee@devsisters.com>
2022-07-22 18:49:27 +0900

    feat: Moya Network Layer 추가

---
+++ JYP-iOS/JYP-iOS/Sources/Network/API/BaseAPI.swift
@@ -0,0 +1,28 @@
+//
+//  BaseAPI.swift
+//  JYP-iOSTests
+//
+//  Created by inae Lee on 2022/07/21.
+//  Copyright © 2022 JYP-iOS. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+protocol BaseAPI: TargetType {}
+
+extension BaseAPI {
+    var baseURL: URL {
+        URL(string: Environment.url)!
+    }
+
+    var headers: [String: String]? {
+        [
+            "Content-Type": "application/json"
+        ]
+    }
+
+    var sampleData: Data {
+        Data()
+    }
+}
