bee499143f3260b7dfd63d4244059a370297ae15
inae <inae.lee@devsisters.com>
2022-08-20 02:40:54 +0900

    chore: model 변경사항 대응

--- JYP-iOS/JYP-iOS/Sources/Presenter/MyPlanner/ScheduledJourney/ScheduledJourneyReactor.swift
+++ JYP-iOS/JYP-iOS/Sources/Presenter/MyPlanner/ScheduledJourney/ScheduledJourneyReactor.swift
@@ -21,7 +21,7 @@ final class ScheduledJourneyReactor: Reactor {
     init() {
 //        let section = ScheduledJourneySectionModel(model: (), items: [.empty])
 
-        let journey = Journey(id: "1", member: [], name: "name", startDate: 0.0, endDate: 0.0, pikis: [], tags: [], candidatePlaces: [], themeUrl: "url")
+        let journey = Journey(id: "1", member: [], name: "name", startDate: 0.0, endDate: 0.0, journeyPlaces: [], tags: [], candidatePlaces: [], themeUrl: "url")
 
         let section = ScheduledJourneySectionModel(model: (), items: [.journey(.init(journey: journey)), .journey(.init(journey: journey)), .journey(.init(journey: journey))])
 
