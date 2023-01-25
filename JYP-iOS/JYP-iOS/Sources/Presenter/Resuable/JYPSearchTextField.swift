ed84eab0934eef5c3f7f249bcff2ca22446ea0cd
inae <inae.lee@devsisters.com>
2023-01-25 16:43:34 +0900

    fix: textField의 값 변경을 감지해 trailingButton을 조절함

--- JYP-iOS/JYP-iOS/Sources/Presenter/Resuable/JYPSearchTextField.swift
+++ JYP-iOS/JYP-iOS/Sources/Presenter/Resuable/JYPSearchTextField.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2022 JYP-iOS. All rights reserved.
 //
 
+import RxCocoa
+import RxSwift
 import UIKit
 
 enum JYPSearchTextFieldType {
@@ -13,7 +15,7 @@ enum JYPSearchTextFieldType {
     case region
     case tag
     case planner
-    
+
     var config: JYPSeachTextFieldConfig {
         switch self {
         case .place:
@@ -26,7 +28,7 @@ enum JYPSearchTextFieldType {
             return .init(placeholderText: "예) 제주도 여행기", font: JYPIOSFontFamily.Pretendard.semiBold.font(size: 24), textColor: JYPIOSAsset.textB90.color, backgroundColor: JYPIOSAsset.backgroundWhite100.color)
         }
     }
-    
+
     var trailingIcon: UIImage {
         switch self {
         case .place, .region:
@@ -35,7 +37,7 @@ enum JYPSearchTextFieldType {
             return UIImage()
         }
     }
-    
+
     var isHiddenBottomBorder: Bool {
         switch self {
         case .place, .region: return true
@@ -51,82 +53,86 @@ struct JYPSeachTextFieldConfig {
     let backgroundColor: UIColor
 }
 
-class JYPSearchTextField: BaseView {
+final class JYPSearchTextField: BaseView {
     let type: JYPSearchTextFieldType
     let textField = UITextField()
     let trailingButton = UIButton()
     let bottomBorderLine = UIView()
-    
+
+    private lazy var textfieldValueChangedEvent = Observable.merge(
+        textField.rx.changedText.asObservable(),
+        textField.rx.observe(String.self, "text")
+    ).asObservable().compactMap { $0 }
+
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("not supported")
     }
-    
+
     init(type: JYPSearchTextFieldType) {
         self.type = type
-        
+
         super.init(frame: .zero)
     }
-    
+
     override func setupProperty() {
         super.setupProperty()
-        
+
         backgroundColor = type.config.backgroundColor
-        
+
         textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 16, height: textField.frame.height))
         textField.leftViewMode = .always
         textField.tintColor = JYPIOSAsset.textB90.color
         textField.placeholder = type.config.placeholderText
         textField.font = type.config.font
         textField.textColor = type.config.textColor
-        
+
         trailingButton.setImage(type.trailingIcon, for: .normal)
         trailingButton.setImage(JYPIOSAsset.iconTextDelete.image, for: .selected)
-        
+
         bottomBorderLine.backgroundColor = .black.withAlphaComponent(0.1)
         bottomBorderLine.isHidden = type.isHiddenBottomBorder
     }
-    
+
     override func setupHierarchy() {
         super.setupHierarchy()
-        
+
         addSubviews([textField, trailingButton, bottomBorderLine])
     }
-    
+
     override func setupLayout() {
         super.setupLayout()
-        
+
         textField.snp.makeConstraints {
             $0.top.leading.bottom.equalToSuperview()
             $0.trailing.equalTo(trailingButton.snp.leading).offset(-6)
         }
-        
+
         trailingButton.snp.makeConstraints {
             $0.trailing.equalToSuperview().inset(12)
             $0.centerY.equalToSuperview()
             $0.width.height.equalTo(20)
         }
-        
+
         bottomBorderLine.snp.makeConstraints { make in
             make.bottom.equalTo(textField.snp.bottom)
             make.leading.trailing.equalToSuperview()
             make.height.equalTo(1)
         }
     }
-    
+
     override func setupBind() {
         super.setupBind()
-        
-        textField.rx.text.orEmpty
+
+        textfieldValueChangedEvent
+            .distinctUntilChanged()
+            .map { !$0.isEmpty }
             .withUnretained(self)
-            .bind { this, text in
-                if text.isEmpty {
-                    this.trailingButton.isSelected = false
-                } else {
-                    this.trailingButton.isSelected = true
-                }
+            .bind { this, isNotEmpty in
+                this.trailingButton.isSelected = isNotEmpty
             }
             .disposed(by: disposeBag)
-        
+
         trailingButton.rx.tap
             .withUnretained(self)
             .bind { this, _ in
@@ -137,7 +143,7 @@ class JYPSearchTextField: BaseView {
             }
             .disposed(by: disposeBag)
     }
-    
+
     func setupToolBar() {
         let toolbar = UIToolbar()
         let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
@@ -148,9 +154,9 @@ class JYPSearchTextField: BaseView {
         toolbar.updateConstraintsIfNeeded()
         textField.inputAccessoryView = toolbar
     }
-    
+
     @objc
-    func didTapDoneButton(_ sender: UIBarButtonItem) {
-        self.textField.resignFirstResponder()
+    private func didTapDoneButton(_ sender: UIBarButtonItem) {
+        textField.resignFirstResponder()
     }
 }
