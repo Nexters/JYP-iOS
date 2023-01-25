190e539eca189e737eff5b020e02228fa41b8bf1
inae <inae.lee@devsisters.com>
2023-01-25 16:43:05 +0900

    feat: changedText event 생성

--- JYP-iOS/JYP-iOS/Sources/Extension/Rx+Ext.swift
+++ JYP-iOS/JYP-iOS/Sources/Extension/Rx+Ext.swift
@@ -10,7 +10,7 @@ import RxCocoa
 import RxSwift
 import UIKit
 
-public extension Reactive where Base: UIViewController {
+extension Reactive where Base: UIViewController {
     var viewDidLoad: ControlEvent<Void> {
         let source = methodInvoked(#selector(Base.viewDidLoad)).map { _ in }
         return ControlEvent(events: source)
@@ -20,9 +20,26 @@ public extension Reactive where Base: UIViewController {
         let source = methodInvoked(#selector(Base.viewWillAppear)).map { $0.first as? Bool ?? false }
         return ControlEvent(events: source)
     }
-    
+
     var viewDidAppear: ControlEvent<Bool> {
         let source = methodInvoked(#selector(Base.viewDidAppear)).map { $0.first as? Bool ?? false }
         return ControlEvent(events: source)
+    }
+}
+
+extension Reactive where Base: UITextField {
+    /// textField의 focus 이벤트를 제외하고 text 값이 변경될 때 방출된다
+    var changedText: ControlProperty<String?> {
+        base.rx.controlProperty(
+            editingEvents: [.editingChanged, .valueChanged],
+            getter: { textField in
+                textField.text
+            },
+            setter: { textField, value in
+                if textField.text != value {
+                    textField.text = value
+                }
+            }
+        )
     }
 }
